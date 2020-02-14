9e47f2d976eda192c7852dc214c939f16f008b05
Georgmany Ruiz <37196080+GeorgmanyRuiz@users.noreply.github.com>
2020-02-14 14:50:22 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function myFunction() {
+
+      for (var x=0; x<=100; x++) {
+
+          if (x === 0) {
+                  document.write("Dallas ");
+
+          }
+          else if (x % 2 === 0 ) {
+                  document.write(" Boys")
+          }
+
+          else  {
+                  document.write(" Cow")
+          }
+
+          if (x % 3 <= 0 ) {
+             document.write(" Cowboys")
+          }
+        }
+}
